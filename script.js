84d25381f6491a77fb89de325eb560a182503e58
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-02 20:10:57 -0700

    Add display default 0

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 (function () {
-  const operators = {
-    '+': (a, b) => Number(a) + Number(b), // to avoid a + b = 'ab'
-    '-': (a, b) => a - b,
-    '*': (a, b) => a * b,
-    '/': (a, b) => a / b,
-  }
+  /* "global" variables */
+  let displayContent = '';
 
   /* elements */
   const currentDisplay = document.querySelector('.display .current-display');
@@ -12,21 +8,31 @@
   const numberButtons = document.querySelectorAll('.calc-buttons .num-button');
   const clearButton = document.querySelector('.calc-buttons .clear');
 
+
+  const operators = {
+    '+': (a, b) => Number(a) + Number(b), // to avoid a + b = 'ab'
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+  }
+
   /* event listener functions */
   function operate(var1, operator, var2) {
     return operators[operator](var1, var2);
   }
 
   function addNumberToDisplay() {
-    let numberAdded = this.textContent;
-    currentDisplay.textContent += numberAdded;
+    let numberPressed = this.textContent;
+    if (currentDisplay.textContent === '0') currentDisplay.textContent = numberPressed;
+    else currentDisplay.textContent += numberPressed;
+    displayContent = currentDisplay.textContent;
   }
 
   function clearDisplay() {
-    currentDisplay.textContent = '';
+    currentDisplay.textContent = '0';
     previousDisplay.textContent = '';
+    displayContent = '';
   }
-  
 
   /* event listeners */
   numberButtons.forEach(button => button.addEventListener('click', addNumberToDisplay));
